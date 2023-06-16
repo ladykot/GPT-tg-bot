f2c64a74b2e56edf9d829e369a59584fd9600876
Katya Akilina <katyaartline@gmail.com>
2023-06-16 17:24:01 +0300

    feat: add session

--- src/main.js
+++ src/main.js
@@ -1,29 +1,52 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, session } from 'telegraf';
 import { message } from 'telegraf/filters';
 import { code } from 'telegraf/format';
 import config from 'config';
 import { ogg } from './converter.js';
 import { openai } from './openai.js';
 
+const INITIAL_SESSION = {
+  messages: [],
+};
+
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
 
+bot.use(session()); // чтобы запомнить контекст разговора
+
+// команда д/создания новой сессии
+bot.command('new', async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply('... Чем могу помочь?');
+});
+
 bot.on(message('voice'), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION;
   try {
     await ctx.reply(code('(∩^o^)⊃━☆ﾟ.*･｡ﾟ...'));
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id); // get link to .ogg file
     const userId = String(ctx.message.from.id);
     const oggPath = await ogg.create(link.href, userId);
     const mp3Path = await ogg.convertToMp3(oggPath, userId);
+
     const text = await openai.transcription(mp3Path);
-    const messages = [{ role: openai.roles.USER, content: text }];
-    const response = await openai.chat(messages);
+
+    ctx.session.messages.push({ role: openai.roles.USER, content: text });
+
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
     await ctx.reply(response.content);
   } catch (error) {
     console.log('Error while voice message', error.message);
   }
 });
 
+
 bot.command('start', async (ctx) => {
+  ctx.session = INITIAL_SESSION;
   // reply to user
   await ctx.reply(JSON.stringify(ctx.message, null, 2)); // ctx - current info about bot's state
 });
